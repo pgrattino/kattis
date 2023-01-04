a3e2ed6363dad6ba5143e3b9abe8c593c5c641c4
Peyton Grattino <grattinopeyton@gmail.com>
2023-01-04 02:57:30 -0700

    added carrots

---
+++ python/carrots/carrots.py
@@ -0,0 +1,11 @@
+def main():
+    list = input()
+    a, b = list.split()
+    a = int(a)
+    b = int(b)
+    for i in range(a):
+        userStr = input()
+    print(b)
+
+if __name__ == "__main__":
+    main()
