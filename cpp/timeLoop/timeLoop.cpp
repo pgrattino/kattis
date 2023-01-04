58e19e50d57cb226b31a36b953983cf33e07cbb7
Peyton Grattino <grattinopeyton@gmail.com>
2023-01-04 02:06:05 -0700

    added timeLoop

---
+++ cpp/timeLoop/timeLoop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int userInt;
+	cin >> userInt;
+	for (int i = 1; i <= userInt; i++){
+		printf ("%i Abracadabra\n", i);
+	}
+	return 0;
+}
